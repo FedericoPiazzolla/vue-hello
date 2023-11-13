9e97eba089d260898b7f174eb803ef915d0f5266
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-13 16:58:54 +0100

    add: text and img

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-"https://picsum.photos/200/300";
+// "https://picsum.photos/200/300";
 const { createApp } = Vue;
+const app = createApp({
+
+  data() {
+
+    return {
+      greetingText: "ciao user",
+      img: "https://picsum.photos/200/300",
+    }
+
+  },
+
+
+});
+
+app.mount("#app")
